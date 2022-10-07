e8127ae6405443ab01c6ce5b3bc9e00f6321ad50
an.mangone <an.mangone@reply.it>
2022-10-07 12:20:37 +0200

    Manage KeyEvents (show right/wrong character

--- script.js
+++ script.js
@@ -26,36 +26,73 @@ const wordList = [
 const inputs = document.querySelector(".inputs"),
   hintLabel = document.querySelector(".hint-label"),
   remainingGuessesLabel = document.querySelector(".remaining-guesses-label"),
+  wrongCharactersLabel = document.querySelector(".wrong-characters-label"),
   resetBtn = document.querySelector(".reset-btn");
+
+var wrongCharacters = [];
 
 function startGame() {
   // Retrieve random word from wordList
-  let randomObj = getRandomWord(),
+  var randomObj = getRandomWord(),
     hint = randomObj ? randomObj.hint : null,
     word = randomObj ? randomObj.word : null;
 
-  if (randomObj) {
-    if (hintLabel) {
-      hintLabel.innerHTML = "Hint: " + "<b>" + hint + "</b>";
-    }
+  initFields(hint);
 
-    if (inputs) {
-      // ! Clean child's
-      inputs.innerHTML = "";
+  wrongCharacters = [];
 
-      for (let i = 0; i < word.length; i++) {
-        // Create input element
-        let input = document.createElement("input");
-        // Set CSS
-        input.setAttribute("class", ".inputs input");
+  for (let i = 0; i < word.length; i++) {
+    // Create input element
+    let input = document.createElement("input");
+    // Set CSS
+    input.setAttribute("class", ".inputs input");
 
-        input.disabled = true;
-        input.value = word.charAt(i);
+    input.disabled = true;
+    input.value = word.charAt(i).toLocaleLowerCase();
 
-        inputs.append(input);
-      }
-    }
+    // Hide character
+    input.style.color = "transparent";
+
+    inputs.append(input);
   }
+
+  // Key events
+  document.addEventListener(
+    "keydown",
+    (event) => {
+      var key = event.key;
+
+      // ! Allow to digit only characters and numbers
+      if (!isKeyValid(key)) {
+        alert("Please, digit a valid character or number!");
+        return;
+      }
+
+      // ? Check if word contains key pressed
+      if (word.toLocaleLowerCase().includes(key)) {
+        // Case when word contains key:
+        // # Show character
+        (inputs.childNodes || []).forEach((input) => {
+          if (input.value === key) {
+            input.style.color = "#57a6a8";
+          }
+        });
+      } else {
+        // Case when word not contains key:
+        // # Add character to wrongCharactersLabel
+        if (!wrongCharacters.includes(key)) {
+          wrongCharacters.push(key);
+          wrongCharactersLabel.innerHTML =
+            "Wrong characters: " +
+            "<b>" +
+            wrongCharacters.join(", ").toLocaleUpperCase() +
+            "</b>";
+        }
+        // TODO Decrement remaining guesses
+      }
+    },
+    false
+  );
 }
 
 startGame();
@@ -67,6 +104,33 @@ if (resetBtn) {
   });
 }
 
+function isKeyValid(key) {
+  if (key.toLowerCase().length !== 1) {
+    return false;
+  }
+
+  const isLetter = key >= "a" && key <= "z";
+  const isNumber = key >= "0" && key <= "9";
+
+  if (isLetter || isNumber) {
+    return true;
+  }
+
+  return false;
+}
+
 function getRandomWord() {
   return wordList[Math.floor(Math.random() * wordList.length)];
+}
+
+function initFields(hint) {
+  if (inputs) {
+    inputs.innerHTML = "";
+  }
+  if (hintLabel) {
+    hintLabel.innerHTML = "Hint: " + "<b>" + hint + "</b>";
+  }
+  if (wrongCharactersLabel) {
+    wrongCharactersLabel.innerHTML = "Wrong characters: ";
+  }
 }
